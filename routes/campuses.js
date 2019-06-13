ca70862474c97aa2d690cb7f66112e8898718486
Jasper Cheung <wymanjasper@gmail.com>
2019-06-13 14:38:10 -0400

    added all of campus routes

--- routes/campuses.js
+++ routes/campuses.js
@@ -4,21 +4,21 @@ const { Campus } = require('../database/models');
 
 
 router.use(bodyParser.json());
-
+//serves up all campuses
 router.get("/", (req, res, next) => {
   {
     Campus.findAll()
       .then(campus => res.json(campus))
       .catch(next);
   }
- // const allCampus =  Campus.findAll();
- // console.log(allCampus);
+  // const allCampus =  Campus.findAll();
+  // console.log(allCampus);
 
 });
-
- router.get('/:id', async (req, res, next) => {
+//serves up campus with id
+router.get('/:id', async(req, res, next) => {
   try {
-    //  SELECT * FROM students WHERE id = 'req.params.id'
+    //  SELECT * FROM campus WHERE id = 'req.params.id'
     let campus = await Campus.findByPk(req.params.id);
     if (campus) {
       res.json(campus);
@@ -29,12 +29,45 @@ router.get("/", (req, res, next) => {
     next(error);
   }
 });
-
-
-router.post("/submit", (req,res,next) => {
-  console.log(req.body);
-  res.json(req.body);
+//add new campus
+router.post('/', async (req, res, next) => {
+  try {
+    //  INSERT INTO students ("col1", "col2") VALUES ('val1', 'val2')
+    let campus = await Campus.create(req.body);
+    res.status(201).json(campus);
+  } catch (err) {
+    next(err);
+  }
 });
+//delete campus
+router.delete('/:id', async (req, res, next) => {
+  try {
+    //  DELETE FROM campus WHERE id = req.prams.id
+    const deleteCount = await Campus.destroy({
+      where: { id: req.params.id },
+    });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+//edit campus
+router.put('/:id', async (req, res, next) => {
+  try {
+    //  UPDATE campuses SET "col1" = 'val1', "col2" = 'val2' WHERE id = req.params.id
+    let updatedCampusInfo = await Campus.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+      plain: true,
+    });
+    res.json(updatedCampusInfo[1]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+//edit campus information (including adding/removing students)
 
 
 // router.post;
